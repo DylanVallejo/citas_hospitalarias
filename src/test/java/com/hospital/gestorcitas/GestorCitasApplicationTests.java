881ca63a63e0646ff7daf7f274423e30dda34874
DylanVallejo <dylan1811vallejo@gmail.com>
2024-04-19 00:34:50 -0500

    first commit proyecto gestor hospitalario - se adiciona la configruacion para conexiona a base

---
+++ src/test/java/com/hospital/gestorcitas/GestorCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.gestorcitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
